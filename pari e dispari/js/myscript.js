d1386e99f09b129c0edfad7a5af48b18bf74b551
Sabelius <sbebby92@virgilio.it>
2021-08-31 15:26:50 +0200

    versione finale

--- pari e dispari/js/myscript.js
+++ pari e dispari/js/myscript.js
@@ -1,3 +1,4 @@
+let parola = prompt("Scegli pari o dispari:");
 let numeroUtente = parseInt(prompt("Scegli pari o dispari inserendo un numero:"));
 let somma = 0;
 
@@ -6,29 +7,30 @@ if (numeroUtente > 5 || numeroUtente < 0) {
     console.log("Inserisci un numero compreso tra 0 e 5");
 } else {
     console.log("Il numero scelto dall'utente è:" + numeroUtente);
+    let numeroComputer = getRandomNumber(0, 5);
+    console.log("Il numero estratto dal computer è:" + numeroComputer);
+
+    somma = numeroUtente + numeroComputer;
+    console.log("La sommma dei due numeri è:" + somma);
+
+
+    if (vittoria(somma) == parola) {
+        console.log("Il vincitore è l'utente!!!");
+    } else {
+        console.log("Il vincitore è il computer!!!");
+    }
+
 }
 
 function getRandomNumber(min, max) {
     return (Math.floor(Math.random() * (max - min + 1) + min));
 }
 
-let numeroComputer = getRandomNumber(0, 5);
-console.log("Il numero estratto dal computer è:" + numeroComputer);
-
-somma = numeroUtente + numeroComputer;
-console.log("La sommma dei due numeri è:" + somma);
-
-
-function vittoria (vincitore){
-   if(vincitore == somma){
-       return true;
-   }
-   return false;
+function vittoria(sum) {
+    if (sum % 2 == 0) {
+        return "pari";
+    }
+    return "dispari";
 }
 
-if(vittoria(numeroUtente)){
-    console.log("Il vincitore è l'utente!!!");
-}else{
-    console.log("Il vincitore è il computer!!!");
-}
 
